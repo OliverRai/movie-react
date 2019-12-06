306b3438cc90ca4da8532a68e5f5d35df552c188
OliverRai <raiane.mateus.oliveira@gmail.com>
2019-12-06 20:32:02 -0300

    estelizando home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,49 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './home.css'
 
 
 class Home extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            movies: []
+        }
+        this.loadmovie = this.loadmovie.bind(this);
+    }
+
+    componentDidMount(){
+        this.loadmovie();
+    }
+
+    loadmovie(){
+        //url da API https://sujeitoprogramador.com/ri-api/?api=filmes/
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+        fetch(url)
+        .then((r) => r.json())
+        .then((jason)=> {
+            this.setState({movies: jason});
+            console.log(jason);
+        });
+
+    }
+
     render(){
         return(
-            <div>
-                Pagina Home
+            <div className="container">
+                <div className="lista-filme">
+                    {this.state.movies.map((movie) =>{
+                        return(
+                            <article key={movie.id} className="filme">
+                                <strong>{movie.nome}</strong>
+                                <img src={movie.foto} alt="capa"/>
+                                <Link to="/">Acessar</Link>
+                            </article>
+                        )
+
+                    })}
+                </div>
             </div>
         );
     }
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Home from './pages/Home'
+import Header from './components/Header';
 
 const Routes = () => {
     return(
         <BrowserRouter>
+            <Header></Header>
             <Switch>
                 <Route exact path="/" component={Home}/>
             </Switch>
